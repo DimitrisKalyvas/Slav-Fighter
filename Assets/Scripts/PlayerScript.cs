2157b6948cf29de3826e77611eef554317dbfd41
Dimitris-Marios Kalyvas (Laptop) <dimitrismarioskalyvas@gmail.com>
2022-07-11 17:37:15 +0300

    Fixed camera bug and jumping now works as intended

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -2,50 +2,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerScript : MonoBehaviour
-{
+public class PlayerScript : MonoBehaviour {
+    // SerializeField variables
     [SerializeField] int speedX = 10;
     [SerializeField] int jumpForce = 10;
+    [SerializeField] int maxJumps = 2;
 
+    // Public variables
     public Rigidbody2D rb;
+    public bool isTouchingGround = true;
+    public bool hasDoubleJumped = false;
+    public Vector3 Movement;
 
-    private bool isTouchingGround = false;
-    private bool hasDoubleJumped = false;
+    // Private variables
+    private int jumps = 0;
 
     // Update is called once per frame
-    void Update(){
+    void Update() {
         // Left-Right movement
         float InputX = Input.GetAxis("Horizontal");
+        Movement = new Vector3(InputX * speedX, 0, 0);
 
-        Vector3 Movement = new Vector3(InputX * speedX, 0, 0);
-
-        // Double Jump 
-        if (Input.GetKeyDown(KeyCode.Space)){
-            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-            if ((!isTouchingGround)&&(!hasDoubleJumped)){
-                if (Input.GetKeyDown(KeyCode.Space)){
-                    Movement.y = 0;
-                    rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-                    hasDoubleJumped = true;
-                }
-            }
+        // Enter Jump method
+        if (Input.GetKeyDown(KeyCode.Space)||(Input.GetKeyDown(KeyCode.W))||(Input.GetKeyDown(KeyCode.UpArrow))) {
+            Jump();
         }
         Movement = Movement * Time.deltaTime;
-        transform.Translate(Movement);   
-    }
+        transform.Translate(Movement);
+        }
 
-    void OnCollisionEnter2D(Collision2D collision){
-        if(collision.gameObject.CompareTag("Ground")){
-            // Reset "Jump" booleans
+    // Player can jump for "maxJumps" times allowed
+    void Jump() {
+        if (jumps > 0) { //Check if player has jumped for the maximum times, then jumps. Otherwise, falls and resets boolean
+            rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
             isTouchingGround = true;
-            hasDoubleJumped = false;
+            jumps--;
         }
     }
-
-    void OnCollisionExit2D(Collision2D collision){
-        if(collision.gameObject.CompareTag("Ground")){
-            isTouchingGround = false;
-            hasDoubleJumped = false;
-        }   
-    }
+    void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.CompareTag("Ground")) {
+            // Reset "Jump" related booleans
+            isTouchingGround = true;
+            jumps = maxJumps;
+        }
+    }       
 }
